e44942a074ebe5655eb5fcd5c40b82b640fd2d61
Samuel Wilk <samuel.wilk.00@gmail.com>
2024-05-08 13:00:45 +0200

    Rename host 1blu

--- system/blu/configuration.nix
+++ system/blu/configuration.nix
@@ -18,7 +18,7 @@
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "1blu"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
