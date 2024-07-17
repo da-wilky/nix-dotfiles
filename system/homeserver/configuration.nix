0c2fd0755bca8669fab171c43067e7bed7347a34
Samuel Wilk <34423885+da-wilky@users.noreply.github.com>
2024-07-17 20:00:27 +0200

    Add netbird config

--- system/homeserver/configuration.nix
+++ system/homeserver/configuration.nix
@@ -38,6 +38,14 @@ in {
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFYPJIYpDXdLFLTzp+ftjjA9dgS1jAw2URGM15LTRUzI fabius2001@hotmail.de" ];   
   };
 
+  services.netbird = {
+    enable = true;
+    tunnels.mine.environment = {
+      NB_MANAGEMENT_URL = "https://netbird.swilk.eu";
+      NB_SETUP_KEY = "16EBCBC9-0167-47C2-B7BF-C4EAC0BD108F";
+    };
+  };
+
   #environment.variables = pkgs.lib.mkForce {
   #	"NIX_LD_LIBRARY_PATH" = lib.makeLibraryPath [
   #       pkgs.stdenv.cc.cc
