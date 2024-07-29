467afb4a136904fb2bd2db149d7ff9f9310b80e1
Samuel Wilk <samuel.wilk.00@gmail.com>
2024-07-29 21:05:07 +0200

    Added pi to secrets and wlan

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,8 +6,10 @@ let
   # Systems
   blu = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICaN+jZ5BDtdWRWlZMZdgnsKNCrw62LzD5T5MVZf+Lco";
   home = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE13KcqVy6GOB/ZOFf9QoSP+jSFpRBJ2dE0jjXx/bdzv";
-  systems = [ blu home ];
+  pibackups = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ55y5rKMDqYQThu6uoVGicHpnFT6d3Cm/fGu4LWFDSJ";
+  systems = [ blu home pibackups ];
 in
 {
   "samuel-ssh.age".publicKeys = [ samuel ] ++ systems;
+  "dd-wlan.age".publicKeys = [ samuel pibackups ];
 }
