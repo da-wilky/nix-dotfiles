518bdd09ac7ccfa398845f17f6e9cf9d655af388
Samuel Wilk <samuel.wilk.00@gmail.com>
2025-01-27 22:40:57 +0100

    No docker dns servers

--- modules/docker.nix
+++ modules/docker.nix
@@ -5,7 +5,7 @@
     enable = true;
     liveRestore = false;
   };
-  virtualisation.docker.daemon.settings = {
-    dns = [ "8.8.8.8" "8.8.4.4" ];
-  };
+  #virtualisation.docker.daemon.settings = {
+  #  dns = [ "8.8.8.8" "8.8.4.4" ];
+  #};
 }
