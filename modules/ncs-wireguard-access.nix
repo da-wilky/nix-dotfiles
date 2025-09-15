5a281a36cae2164a03a23c6921864ff8d01ad3f0
Samuel Wilk <34423885+da-wilky@users.noreply.github.com>
2025-09-15 20:38:13 +0200

    Add ncsystems secret

--- modules/ncs-wireguard-access.nix
+++ modules/ncs-wireguard-access.nix
@@ -1,14 +1,25 @@
 { config, ... }@inputs: 
 
+let
+  sopsFile = ../ncsystems-secrets.yml;
+in
 {
-  sops.secrets.nc-systems-wireguard-config = {
-    sopsFile = ../ncsystems-secrets.yml;
+  sops = {
+    secrets = {
+      nc-systems-wireguard-config = {
+	inherit sopsFile;
+      };
+      nc-systems-wireguard-172-config = {
+	inherit sopsFile;
+      };
+    };
   };
 
   networking = {
     wg-quick = {
       interfaces = {
 	wgnc.configFile = config.sops.secrets.nc-systems-wireguard-config.path; 
+	wgnci.configFile = config.sops.secrets.nc-systems-wireguard-172-config.path;
       };
     };
   };
